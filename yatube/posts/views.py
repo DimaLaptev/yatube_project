f189978133b020dc37df465e6803a9d5cec795ef
Dmitriy Shipilov <shipilenok1@gmail.com>
2022-11-30 15:16:10 +0300

    Add urls and view-functions.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse('This is homepage, sun.')
+
+def group_posts(request, slug):
+    return HttpResponse(f'Post {slug} are verified by RosKomControl!')
