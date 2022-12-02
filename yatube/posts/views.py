351f7704bd5763d964ca331c317c5d862539965c
Dmitriy Shipilov <shipilenok1@gmail.com>
2022-12-02 19:23:03 +0300

    Add model "Group", class PostAdmin with admin-view DB, add data rendering in .html-files

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
-# Create your views here.
 def index(request):
-    template = 'posts/index.html'
-    text_main = 'Это главная страница проекта Yatube'
-    context: dict = {'text_main': text_main}
-    return render(request, template, context)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context: dict = {
+        'posts': posts,
+        'title': 'This is main page of Yatube'
+    }
+    return render(request, 'posts/index.html', context)
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
-    text_group = 'Здесь будет информация о группах проекта Yatube'
-    context: dict = {'text_group': text_group}
-    return render(request, template, context)
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context: dict = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
